a1460afcf60c86bdb80de5ac1f37e813eab31750
gealit <gealit0369@yandex.ru>
2023-02-05 12:44:35 +0300

    Customized django admin pages, listing, adding and changing users.

--- app/core/admin.py
+++ app/core/admin.py
@@ -26,7 +26,7 @@ class UserAdmin(BaseUserAdmin):
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
-    readonly_fields = ['last_login',]
+    readonly_fields = ['last_login', ]
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
@@ -41,5 +41,6 @@ class UserAdmin(BaseUserAdmin):
             )
         }),
     )
+
 
 admin.site.register(User, UserAdmin)
